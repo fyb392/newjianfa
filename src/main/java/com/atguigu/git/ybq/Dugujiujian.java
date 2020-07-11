e84c232f831902fc95705033b0caffd66c40a2c3
fyb <fyb_392@163.com>
2020-07-11 10:51:06 +0800

    create

---
+++ src/main/java/com/atguigu/git/ybq/Dugujiujian.java
@@ -0,0 +1,15 @@
+package com.atguigu.git.ybq;
+
+/**
+ * @author fyb
+ * @create 2020-07-11-10:48
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+        System.out.println("dugujiujian");
+    }
+}
