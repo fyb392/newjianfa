c5fe0f1e62713078dd13c748e5db3c46bafc6030
fyb <fyb_392@126.com>
2020-07-12 22:02:46 +0800

    updata_lhc

--- src/main/java/com/atguigu/git/ybq/Dugujiujian.java
+++ src/main/java/com/atguigu/git/ybq/Dugujiujian.java
@@ -12,5 +12,12 @@ public class Dugujiujian {
         System.out.println("dugujiujian");
         System.out.println("dugujiujian");
         System.out.println("1111111111111111");
+        System.out.println("22222222222222222222");
+        System.out.println("22222222222222222222");
+        System.out.println("22222222222222222222");
+        System.out.println("22222222222222222222");
+        System.out.println("22222222222222222222");
+        System.out.println("22222222222222222222");
+
     }
 }
