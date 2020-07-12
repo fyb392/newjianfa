7dac8560d3a1f202e50bf00e10ecb670705183d0
fyb392 <fyb_392@163.com>
2020-07-12 22:51:42 +0800

    bixiejianfa_create

---
+++ src/main/java/com/atguigu/git/riyue/Bixiejianfa.java
@@ -0,0 +1,13 @@
+package com.atguigu.git.riyue;
+
+/**
+ * @author fyb
+ * @create 2020-07-12-22:49
+ */
+public class Bixiejianfa {
+    public static void main(String[] args) {
+        System.out.println("bixiejianfahao");
+        System.out.println("bixiejianfahao");
+        System.out.println("bixiejianfahao");
+    }
+}
